f42634a458bfb092d47af737967004b0f4404516
Ryan Etedali <ryanetedali@gmail.com>
2023-02-24 23:07:25 -0600

    added navBar and Footer to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import HomePage from "./pages/HomePage/HomePage";
-
+import NavBar from "./Components/NavBar/NavBar";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <HomePage />
+      <Footer />
     </div>
   );
 }
