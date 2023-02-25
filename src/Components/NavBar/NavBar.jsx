211fef8dfd94a8f206e83c6ee8173bb707773070
Ryan Etedali <ryanetedali@gmail.com>
2023-02-24 22:58:53 -0600

    NavBar created

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,1 +1,23 @@
 import "./NavBar.css"
+
+const NavBar = () => {
+    return (
+        <div className="navBar">
+          <ul className="una">
+            <li className="brand">
+              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                <b>back home</b>
+              </Link>
+            </li>
+            <li className="provider">
+              <Link to={"/projects"} class="prov">Projects</Link>
+            </li>
+            <li className="provider">
+              <Link to={"/contactme"} class="prov">Contact Me</Link>
+            </li>
+          </ul>
+        </div>
+      );
+}
+ 
+export default NavBar;
