7b05bf67966f678b292b98477ce054e1a0defe0d
Ryan Etedali <ryanetedali@gmail.com>
2023-02-24 23:06:05 -0600

    footer created

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return ( 
+        <h4>developed by Ryan Etedali</h4>
+     );
+}
+ 
+export default Footer;
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Projects = () => {
-    const [user, token] = useAuth();
    
     return pass;
   };
