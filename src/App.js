9d1dd99dc49223b3cd05ce3d7a9733d601c97129
Ryan Etedali <ryanetedali@gmail.com>
2023-03-02 23:35:13 -0600

    creating routes for diffrent pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import HomePage from "./pages/HomePage/HomePage";
 import NavBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
+import ContactMe from "./pages/ContactMe/ContactMe";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+      </Routes>
       <Footer />
     </div>
   );
